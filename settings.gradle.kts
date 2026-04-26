75217bd1cf460014af3d18a5f18783c56f4446ef
Chris Awad <dev@chrisawad.com>
2026-04-26 19:30:13 -0400

    couple small changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,10 +12,11 @@ pluginManagement {
     }
 }
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
     repositories {
         google()
         mavenCentral()
